822d089774737965716f830d3686d8f3d24b0f49
KellyTbn <kellytbn14@gmail.com>
2020-01-03 23:07:58 -0500

    MEJORAS

--- src/com/eulersoft/Main.java
+++ src/com/eulersoft/Main.java
@@ -4,74 +4,87 @@ import java.util.Scanner;
 
 public class Main {
     static Simbolo simbolos[] = new Simbolo[6];
+    static String simbolo;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-	    String listaSimbolos[] = {"F","V","&","|","!","="};
+        String listaSimbolos[] = {"F", "V", "&", "|", "!", "="};
         String listaEstados[];
 
-	    String estado = "OP";
+        String estado = "OP";
 
-	    do {
-            System.out.println("escriba el simbolo");
-            String simbolo = scanner.next();
-
-            switch (estado){
-                case "OP":
-                    listaEstados = new String[]{"FV", "FV", "E", "E", "!N", "E"};
-                    llenarArreglo(listaSimbolos, listaEstados);
-                    estado = retornarEstado(simbolo);
-                    break;
-                case "FV":
-                    listaEstados = new String[]{"E","E","&","|","!DL=","!DL="};
-                    llenarArreglo(listaSimbolos, listaEstados);
-                    estado = retornarEstado(simbolo);
-                    break;
-                case "&":
-                    listaEstados = new String[]{"E","E","OP","E","E","E"};
-                    llenarArreglo(listaSimbolos, listaEstados);
-                    estado = retornarEstado(simbolo);
-                    break;
-                case "|":
-                    listaEstados = new String[]{"E","E","E","OP","E","E"};
-                    llenarArreglo(listaSimbolos, listaEstados);
-                    estado = retornarEstado(simbolo);
-                    break;
-                case "!DL=":
-                    listaEstados = new String[]{"E","E","E","E","E","OP"};
-                    llenarArreglo(listaSimbolos, listaEstados);
-                    estado = retornarEstado(simbolo);
-                    break;
-                case "!N":
-                    listaEstados = new String[]{"FV","FV","E","E","E","E"};
-                    llenarArreglo(listaSimbolos, listaEstados);
-                    estado = retornarEstado(simbolo);
-                    break;
-                case "E":
-                    //listaEstados = new String[]{"E","E","E","E","E","E"};
-                    //llenarArreglo(listaSimbolos, listaEstados);
-                    break;
-                default:
-                    throw new IllegalStateException("Unexpected value: " + estado);
+        do {
+            System.out.println("Escriba el simbolo \n SIMBOLOS PERMITIDOS: (F, V, &, |, !, =) SIN ESPACIOS " +
+                    "\n PARA TERMINAR ESCRIBA 0");
+            simbolo = scanner.next();
+            if (!simbolo.equals("0")) {
+                switch (estado) {
+                    case "OP":
+                        listaEstados = new String[]{"FV", "FV", "E", "E", "!N", "E"};
+                        llenarArreglo(listaSimbolos, listaEstados);
+                        estado = retornarEstado();
+                        break;
+                    case "FV":
+                        listaEstados = new String[]{"E", "E", "&", "|", "!DL=", "!DL="};
+                        llenarArreglo(listaSimbolos, listaEstados);
+                        estado = retornarEstado();
+                        break;
+                    case "&":
+                        listaEstados = new String[]{"E", "E", "OP", "E", "E", "E"};
+                        llenarArreglo(listaSimbolos, listaEstados);
+                        estado = retornarEstado();
+                        break;
+                    case "|":
+                        listaEstados = new String[]{"E", "E", "E", "OP", "E", "E"};
+                        llenarArreglo(listaSimbolos, listaEstados);
+                        estado = retornarEstado();
+                        break;
+                    case "!DL=":
+                        listaEstados = new String[]{"E", "E", "E", "E", "E", "OP"};
+                        llenarArreglo(listaSimbolos, listaEstados);
+                        estado = retornarEstado();
+                        break;
+                    case "!N":
+                        listaEstados = new String[]{"FV", "FV", "E", "E", "E", "E"};
+                        llenarArreglo(listaSimbolos, listaEstados);
+                        estado = retornarEstado();
+                        break;
+                    case "E":
+                        System.out.println("Hay un error en tu expresión");
+                        break;
+                    default:
+                        throw new IllegalStateException("No es un caracter permitido: " + estado);
+                }
             }
-        }while (estado!="E");
+        } while (!simbolo.equals("0"));
 
     }
 
-    private static String retornarEstado(String simbolo) {
+    private static String retornarEstado() {
+        String estado = "";
         for (int i = 0; i < simbolos.length; i++) {
-            if (simbolos[i].getSimbolo().equals(simbolo)){
-                return simbolos[i].getEstado();
+            if (simbolos[i].getSimbolo().equals(simbolo)) {
+                estado = simbolos[i].getEstado();
+                break;
+            } else {
+                estado = "E";
             }
         }
-        return "E";
+        if (estado.equals("E")) {
+            System.out.println("Hay un error en tu expresión");
+            simbolo = "0";
+        }
+        return estado;
     }
 
-    static void llenarArreglo(String[] listaSimbolos, String[] listaEstados){
+    static void llenarArreglo(String[] listaSimbolos, String[] listaEstados) {
         for (int i = 0; i < simbolos.length; i++) {
-            simbolos[i]= new Simbolo();
+            simbolos[i] = new Simbolo();
             simbolos[i].setSimbolo(listaSimbolos[i]);
             simbolos[i].setEstado(listaEstados[i]);
         }
     }
+
+
 }
